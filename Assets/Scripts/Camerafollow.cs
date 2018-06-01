6b484c5c4a6143fc20edc9c5ac6fb4777bca11ee
GAURAV KUMAR <gaurav.grd@gmail.com>
2018-06-01 16:21:03 +0530

    Initial commit

---
+++ Assets/Scripts/Camerafollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerafollow : MonoBehaviour {
+
+	public GameObject ballsphere;
+	private Vector3 distance;
+	// Use this for initialization
+	void Start () {
+		distance  = transform.position - ballsphere.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = distance + ballsphere.transform.position;
+	}
+}
---
+++ Assets/Scripts/MoveBall.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoveBall : MonoBehaviour {
+	Rigidbody rb;
+	public int ballspeed = 0;
+	public int jumpspeed = 0;
+	private bool istouching = true;
+	private int counter;
+	public Text Cointext;
+	public AudioSource asource;
+	public AudioClip aclip;
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		counter = 12;
+		Cointext.text = "COINS: " + counter;
+	}
+
+	void Update () {
+		float Hmove = Input.GetAxis("Horizontal");
+		float Vmove = Input.GetAxis("Vertical");
+
+		Vector3 ballmove = new Vector3(Hmove,0.0f,Vmove);
+		rb.AddForce(ballmove*ballspeed);
+
+		if((Input.GetKey(KeyCode.Space)) && istouching == true){
+			Vector3 balljump = new Vector3(0.0f, 6.0f, 0.0f);
+			rb.AddForce(balljump*jumpspeed);
+		}
+		istouching = false;
+	}
+
+	private void OnCollisionStay(){
+		istouching = true;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Coinstag"))
+		{
+			other.gameObject.SetActive(false);
+			Cointext.text = "COINS: " + counter;
+			counter--;
+			asource.PlayOneShot(aclip);
+			if (counter == 0)
+			{
+				SceneManager.LoadScene("EndScene");
+			}
+		}
+	}
+}
